872815ed70d29ea9b05cfe60477e3fe13c46d4fe
ifeday1 <oladeniifeoluwa123@gmail.com>
2023-10-24 20:33:57 +0100

    Book

--- src/utils/CardData.jsx
+++ src/utils/CardData.jsx
@@ -3,6 +3,16 @@ import Preparing from '../assets/preparing.jpg'
 import Multiply from '../assets/mutiply.jpg'
 import Without from '../assets/without.jpg'
 
+import Becoming from '../assets/Becoming.jpg'
+import Marriage from '../assets/Marriage.webp'
+import Leading from '../assets/Leading.webp'
+import Turing from '../assets/Turning.webp'
+import Power from '../assets/Power.webp'
+import Sex from '../assets/Sex.jpg'
+import Singles from '../assets/Singles.jpg'
+import Soaring from '../assets/Soaring.jpg'
+
+
 const cardData = [
     {
         id: 1,
@@ -39,17 +49,67 @@ const cardData = [
     },
     {
         id: 5,
-        image: "image_url_5",
-        description: "Product 5",
-        price: "$12.00",
-        buttonLabel: "Add to Cart",
+        image: Becoming,
+        description: "BECOMING AN EFFECTIVE CHURCH WORKER",
+        price: "$3.99",
+        buttonLabel: "Buy on Amazon",
+        link: "https://www.amazon.com/BECOMING-EFFECTIVE-CHURCH-WORKER-indispensable-ebook/dp/B08R9YKQKH/ref=sr_1_7?keywords=olusola+oladeni&qid=1698085874&sr=8-7",
     },
     {
         id: 6,
-        image: "image_url_6",
-        description: "Product 6",
-        price: "$18.00",
-        buttonLabel: "Add to Cart",
+        image: Marriage,
+        description: "MARRIAGE WITH A DIFFERENCE",
+        price: "$3.99",
+        buttonLabel: "Buy on Amazon",
+        link: "https://www.amazon.com/MARRIAGE-DIFFERENCE-Reading-Adegbenjo-Anniversary-ebook/dp/B08P293FNJ/ref=sr_1_6?keywords=olusola+oladeni&qid=1698085874&sr=8-6",
+    },
+    {
+        id: 7,
+        image: Leading,
+        description: "LEADING THE CHURCH FOR ABIDING RESULTS",
+        price: "$3.99",
+        buttonLabel: "Buy on Amazon",
+        link: "https://www.amazon.com/LEADING-ABIDING-RESULTS-Olusola-Oladeni-ebook/dp/B08YN4BXKC/ref=sr_1_10?keywords=olusola+oladeni&qid=1698085874&sr=8-10",
+    },
+    {
+        id: 8,
+        image: Power,
+        description: "POWER OF DETERMINATION",
+        price: "$3.99",
+        buttonLabel: "Buy on Amazon",
+        link: "https://www.amazon.com/POWER-DETERMINATION-Powerful-Exposition-Book-ebook/dp/B08YMHMDKX/ref=sr_1_8?keywords=olusola+oladeni&qid=1698085874&sr=8-8",
+    },
+    {
+        id: 9,
+        image: Turing,
+        description: "TURING YOUR LITTLE OIL INTO MULTIPLE OIL",
+        price: "$3.99",
+        buttonLabel: "Buy on Amazon",
+        link: "https://www.amazon.com/TURNING-YOUR-LITTLE-MULTIPLE-WEALTH-ebook/dp/B08YNJS2TX/ref=sr_1_9?keywords=olusola+oladeni&qid=1698085874&sr=8-9",
+    },
+    {
+        id: 10,
+        image: Singles,
+        description: "SINGLES' LOVE, LUST & INFATUATION",
+        price: "$3.99",
+        buttonLabel: "Buy on Amazon",
+        link: "https://www.amazon.com/SINGLES-LOVE-INFATUATION-Esther-Oladeni-ebook/dp/B08P28LRHY/ref=sr_1_2?crid=852PHFPQSHXY&keywords=sola+oladeni&qid=1698173122&sprefix=sola+oladeni%2Caps%2C296&sr=8-2",
+    },
+    {
+        id: 11,
+        image: Soaring,
+        description: "SOARING HIGHER IN LIFE AND MINISTRY",
+        price: "$3.99",
+        buttonLabel: "Buy on Amazon",
+        link: "https://www.amazon.com/SOARING-HIGHER-LIFE-MINISTRY-practically-ebook/dp/B08R7JT97F/ref=sr_1_1?crid=852PHFPQSHXY&keywords=sola+oladeni&qid=1698173994&sprefix=sola+oladeni%2Caps%2C296&sr=8-1",
+    },
+    {
+        id: 12,
+        image: Sex,
+        description: "SEX MARRIAGE SUPER MAGNET",
+        price: "$3.99",
+        buttonLabel: "Buy on Amazon",
+        link: "https://www.amazon.com/MARRIAGE-SUPER-MAGNET-Esther-Oladeni-ebook/dp/B08P7H3TLQ/ref=sr_1_3?crid=852PHFPQSHXY&keywords=sola+oladeni&qid=1698173994&sprefix=sola+oladeni%2Caps%2C296&sr=8-3",
     },
 ];
 

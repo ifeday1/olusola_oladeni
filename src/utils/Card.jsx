5d3ceb2682f74f9b559524a3a0b26915957ec002
ifeday1 <oladeniifeoluwa123@gmail.com>
2023-10-24 19:13:09 +0100

    books

--- src/utils/Card.jsx
+++ src/utils/Card.jsx
@@ -10,13 +10,13 @@ const Card = ({ data }) => {
 
 
     return (
-        <Box bg='white' mx="40px" my="20px" borderWidth="1px" borderRadius="lg" overflow="hidden" width='300px' height='300px'>
+        <Box bg='white' mx="40px" my="20px" borderWidth="1px" borderRadius="lg" overflow="hidden" width='300px' height='350px'>
             <Box mt='30px' display='flex' justifyContent='center'>
                 <Image justifyContent='center' width='80px' src={image} alt={description} />
             </Box>
 
             <VStack p={4} spacing={2}>
-                <Text fontWeight="semibold">{description}</Text>
+                <Text fontWeight="semibold" align='center'>{description}</Text>
                 <Text color="teal.500" fontSize="xl">
                     {price}
                 </Text>
--- src/utils/CardData.jsx
+++ src/utils/CardData.jsx
@@ -1,36 +1,41 @@
 import Making from '../assets/making.jpg'
 import Preparing from '../assets/preparing.jpg'
+import Multiply from '../assets/mutiply.jpg'
+import Without from '../assets/without.jpg'
 
 const cardData = [
     {
         id: 1,
         image: Making,
-        description: "The Good Marriage Better",
-        price: "$10.00",
+        description: "MAKING THE GOOD MARRIAGE BETTER",
+        price: "$4.50",
         buttonLabel: "Buy on Amazon",
         link: " https://www.amazon.com/MAKING-MARRIAGE-BETTER-Olusola-Oladeni-ebook/dp/B0CB77257T/ref=sr_1_2?crid=23GLWYN00CK3O&keywords=olusola+oladeni&qid=1697822966&sprefix=olusola+oladeni+%2Caps%2C265&sr=8-2",
     },
     {
         id: 2,
         image: Preparing,
-        description: "Preparing For A Glorious marriage",
-        price: "$20.00",
-        buttonLabel: "Add to Cart",
+        description: "PREPARING FRO A GLORIOUS MARRIAGE",
+        price: "$4.00",
+        buttonLabel: "Buy on Amazon",
         link: "https://www.amazon.com/PREPARING-GLORIOUS-MARRIAGE-OLUSOLA-OLADENI-ebook/dp/B0BTFVF9JQ/ref=sr_1_4?crid=23GLWYN00CK3O&keywords=olusola+oladeni&qid=1697823004&sprefix=olusola+oladeni+%2Caps%2C265&sr=8-4",
     },
     {
         id: 3,
-        image: "image_url_3",
-        description: "Product 3",
-        price: "$15.00",
-        buttonLabel: "Add to Cart",
+        image: Without,
+        description: "MARRIAGE WITHOUT WEEPING",
+        price: "$4.50",
+        buttonLabel: "Buy on Amazon",
+        link: "https://www.amazon.com/MARRIAGE-WITHOUT-WEEPING-Olusola-Oladeni-ebook/dp/B0BS9XRF83/ref=sr_1_5?keywords=olusola+oladeni&qid=1698085874&sr=8-5 ",
     },
     {
         id: 4,
-        image: "image_url_4",
-        description: "Product 4",
-        price: "$25.00",
-        buttonLabel: "Add to Cart",
+        image: Multiply,
+        description: "MULTIPLY YOUR GOLD",
+        price: "$4.50",
+        buttonLabel: "Buy on Amazon",
+        link: "https://www.amazon.com/MULTIPLY-YOUR-GOLD-Olusola-Oladeni-ebook/dp/B0CFVQHJ7Y/ref=sr_1_1?keywords=olusola+oladeni&qid=1698085056&sr=8-1",
+
     },
     {
         id: 5,

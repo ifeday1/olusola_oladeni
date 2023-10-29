a2e9fdddfbfa63e4b0331da02e14969fe93f3de3
ifeday1 <oladeniifeoluwa123@gmail.com>
2023-10-29 21:22:39 +0100

    books

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -26,11 +26,11 @@ const Books = ({ data }) => {
                 )
                 )}
             </Flex>
-            
-            <WhatsappChat/>
+
+            <WhatsappChat />
             <Footer />
         </>
     )
 }
 
-export default Books
+export default Books;

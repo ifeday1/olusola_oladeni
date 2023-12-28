2bd68200dea4896f018147fbe7c2fb1849d4cf93
ifeoluwa oladeni <oladeniifeoluwa123@gmail.com>
2023-12-28 22:57:16 +0100

    Medis

--- src/pages/Media.jsx
+++ src/pages/Media.jsx
@@ -14,7 +14,6 @@ const Media = () => {
     'https://via.placeholder.com/220',
     'https://via.placeholder.com/170',
     'https://via.placeholder.com/190',
-    'https://via.placeholder.com/240',
   ];
   return (
     <>

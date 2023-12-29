e2efca3370a0c056d863854250994baf6eaf00e3
ifeoluwa oladeni <oladeniifeoluwa123@gmail.com>
2023-12-29 18:53:12 +0100

    Media

--- src/pages/Media.jsx
+++ src/pages/Media.jsx
@@ -13,7 +13,6 @@ const Media = () => {
     'https://via.placeholder.com/180',
     'https://via.placeholder.com/220',
     'https://via.placeholder.com/170',
-    'https://via.placeholder.com/190',
   ];
   return (
     <>

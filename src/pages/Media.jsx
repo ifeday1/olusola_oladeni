d12f6130ce8926e62a91ad25c08205a9768f1625
ifeoluwa oladeni <oladeniifeoluwa123@gmail.com>
2024-01-22 21:06:51 +0100

    Update

--- src/pages/Media.jsx
+++ src/pages/Media.jsx
@@ -19,7 +19,6 @@ const Media = () => {
     'https://via.placeholder.com/250',
     'https://via.placeholder.com/250',
     'https://via.placeholder.com/250',
-    'https://via.placeholder.com/250',
   ];
   return (
     <>

4e45138063a4442478481d3499c60fe1b021d8c4
ifeoluwa oladeni <oladeniifeoluwa123@gmail.com>
2024-01-24 22:35:22 +0100

    update

--- src/pages/Media.jsx
+++ src/pages/Media.jsx
@@ -17,8 +17,6 @@ const Media = () => {
     'https://via.placeholder.com/250',
     'https://via.placeholder.com/250',
     'https://via.placeholder.com/250',
-    'https://via.placeholder.com/250',
-    'https://via.placeholder.com/250',
   ];
   return (
     <>

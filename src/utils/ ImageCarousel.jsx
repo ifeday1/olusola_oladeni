7a955ee42443893a534b4d648bf71e2e06d0067f
ifeoluwa oladeni <oladeniifeoluwa123@gmail.com>
2025-02-04 22:20:25 +0100

    update

--- src/utils/ ImageCarousel.jsx
+++ src/utils/ ImageCarousel.jsx
@@ -4,71 +4,74 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './Carousel.css'; // Create a separate CSS file for styling
-import { Text, } from "@chakra-ui/react";
+import { Text } from '@chakra-ui/react';
 
-import Pre1 from "../assets/p1.jpg"
-import Pre2 from "../assets/p2.jpg"
-import Pre3 from "../assets/p3.jpg"
-import Pre4 from "../assets/p4.jpg"
+import Pre1 from '../assets/p1.jpg';
+import Pre2 from '../assets/p2.jpg';
+import Pre3 from '../assets/p3.jpg';
+import Pre4 from '../assets/p4.jpg';
 
 const ImageCarousel = () => {
-    const settings = {
-        centerMode: true,
-        centerPadding: '0',
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 7000,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                },
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                },
-            },
-        ],
-    };
+  const settings = {
+    centerMode: true,
+    centerPadding: '0',
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 7000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
 
-    return (
-      <div className='carousel-container' style={{ width: '100%' }}>
-        <div className='carousel' style={{ background: 'teal' }}>
-          <Text
-            align='center'
-            fontWeight='bold'
-            fontSize='30px'
-            fontStyle='italic'
-            mt='40px'
-            mb='20px'
-            color='white'
-          >
-            Premarital and Marital Matters{' '}
-          </Text>
+  return (
+    <div className='carousel-container' style={{ width: '100%' }}>
+      <div
+        className='carousel'
+        style={{ background: 'teal.800', width: '100%' }}
+      >
+        <Text
+          align='center'
+          fontWeight='bold'
+          fontSize='30px'
+          fontStyle='italic'
+          mt='40px'
+          mb='20px'
+          color='white'
+        >
+          Premarital and Marital Matters{' '}
+        </Text>
 
-          <Slider {...settings}>
-            <div className='carousel-item'>
-              <img src={Pre1} alt='prep' />
-            </div>
-            <div className='carousel-item'>
-              <img src={Pre2} alt='prep1' />
-            </div>
-            <div className='carousel-item'>
-              <img src={Pre3} alt='prep2' />
-            </div>
-            <div className='carousel-item'>
-              <img src={Pre4} alt='prep3' />
-            </div>
-            {/* Add more images as needed */}
-          </Slider>
-        </div>
+        <Slider {...settings}>
+          <div className='carousel-item'>
+            <img src={Pre1} alt='prep' />
+          </div>
+          <div className='carousel-item'>
+            <img src={Pre2} alt='prep1' />
+          </div>
+          <div className='carousel-item'>
+            <img src={Pre3} alt='prep2' />
+          </div>
+          <div className='carousel-item'>
+            <img src={Pre4} alt='prep3' />
+          </div>
+          {/* Add more images as needed */}
+        </Slider>
       </div>
-    );
+    </div>
+  );
 };
 
 export default ImageCarousel;

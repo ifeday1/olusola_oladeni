5b480e9534c704616064ba7eeecdf6e7d73f4423
ifeoluwa oladeni <oladeniifeoluwa123@gmail.com>
2025-02-11 23:11:00 +0100

    Book

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,36 +1,41 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer/Footer'
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer/Footer';
 
-import { Flex, Box, Text } from "@chakra-ui/react";
-import Card from "../utils/Card";
-import cardData from "../utils/CardData";
+import { Flex, Box, Text } from '@chakra-ui/react';
+import Card from '../utils/Card';
+import cardData from '../utils/CardData';
 import WhatsappChat from '../components/Whatsapp/Index';
 
 const Books = ({ data }) => {
+  return (
+    <>
+      <Navbar />
+      <Box mt='90px' width='100%' height='80px' bg='red'>
+        <Text
+          pt='10px'
+          align='center'
+          color='white'
+          fontSize='40px'
+          fontWeight='bold'
+        >
+          MY BOOKS
+        </Text>
+      </Box>
+      <Flex
+        pt='80px'
+        flexWrap={{ base: 'wrap', md: 'no-wrap' }}
+        justifyContent='center'
+      >
+        {cardData.map((item) => (
+          <Card key={cardData.id} data={item} />
+        ))}
+      </Flex>
 
-
-    return (
-        <>
-            <Navbar />
-            <Box mt='90px' width='100%' height='80px' bg='red'>
-                <Text pt='10px' align='center' color='white' fontSize='40px' fontWeight="bold" >MY BOOKS</Text>
-            </Box>
-            <Flex
-                pt='80px'
-                flexWrap={{ base: "wrap", md: "no-wrap" }} // Wrap on small screens, no wrap on medium and larger screens
-                justifyContent="center"
-            >
-                {cardData.map((item) => (
-                    <Card key={cardData.id} data={item} />
-                )
-                )}
-            </Flex>
-
-            <WhatsappChat />
-            <Footer />
-        </>
-    )
-}
+      <WhatsappChat />
+      <Footer />
+    </>
+  );
+};
 
 export default Books;
